6d29d238ff2fa1a5cb812f76aa1cc26da166b8aa
Alphonse <bimalphadv@gmail.com>
2023-06-18 20:12:24 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - This is my entry point
+ *
+ * Return: Always on success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
